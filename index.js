b191de09fd06456777c7b6d5c640ad8db4aa3e52
Nahuel Nievas <nievasnahuel@gmail.com>
2022-10-30 02:26:15 +0200

    Navbar done

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
 })
+
+// $(document).ready(function(){
+//     $(document).click(
+//       function(){
+//         $("#hamburguer").removeClass("navbar navbar-expand-lg bg-light");
+//       }
+//     );
+//   });
+
+
+
+
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
