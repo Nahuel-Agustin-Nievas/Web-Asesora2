402c4468281ffba35edb455ad14c5d9a9d34f2a5
Nahuel Nievas <nievasnahuel@gmail.com>
2022-11-02 17:05:03 +0100

    animation fixed

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ window.addEventListener("scroll", function(){
 
 
 
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
+  // // Look for .hamburger
+  // var hamburger = document.querySelector(".hamburger");
+  // // On click
+  // hamburger.addEventListener("click", function() {
+  //   // Toggle class "is-active"
+  //   hamburger.classList.toggle("is-active");
+  //   // Do something else, like open/close menu
+  // });
